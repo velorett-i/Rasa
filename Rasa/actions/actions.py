7d3f675307eca3f1b6e079289c238ba94dfd0ec3
velorett-i <58183062+velorett-i@users.noreply.github.com>
2022-05-18 16:50:12 +0100

    Add files via upload

--- Rasa/actions/actions.py
+++ Rasa/actions/actions.py
@@ -27,8 +27,10 @@
 #         return []
 
 from inspect import Attribute
+from re import L
 from typing import Text, List, Any, Dict
 from xml.sax.xmlreader import AttributesImpl
+from matplotlib.pyplot import subplots_adjust
 
 from rasa_sdk import Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
@@ -36,6 +38,8 @@ from rasa_sdk.types import DomainDict
 
 from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
 from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
+
+from rasa_sdk.events import SlotSet
 
 def clean_name(name):
     return "".join([c for c in name if c.isalpha()])
@@ -60,6 +64,32 @@ class ValidateNameForm(FormValidationAction):
             return {"first_name": None}
         return {"first_name": name}
 
+"""
+class SeparateByAge(FormValidationAction):
+
+    def age(self) -> Text:
+        return "validate_age_form"
+
+    def validate_age(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        Validate `age` value.
+
+        # If the person is under 17 = questionario 1 / else = questionario 2
+        age = slot_value
+        if age < 17:
+            dispatcher.utter_message(text="User is 17 or under")
+            return SlotSet("age", "under_17")
+        else:
+            return SlotSet("age", "above_17")
+
+
+"""
+
 class ActionMyKB(ActionQueryKnowledgeBase):
     def __init__(self):
         #load knowledge base with data from the given file
@@ -69,3 +99,7 @@ class ActionMyKB(ActionQueryKnowledgeBase):
             "pais", lambda obj: obj["country"] + " (" + obj["info"] + ")" )
 
         super().__init__(knowledge_base)
+
+        
+
+    
